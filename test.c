00e8edaeaaac5e0e36d0a0c7b918d578f5e911b5
[phild-86-debug] <[phildexter86@gmail.com]>
2024-08-26 13:33:24 -0400

    Added missing variables

--- test.c
+++ test.c
@@ -5,5 +5,9 @@
 int main()
 {
     printf("%s\n", "using git is the best way to start learning the correct way!");
+    int a = 12;
+    int b = 19;
+    int c = a + b;
+    printf("%d\n", c);
     return 0;
 }
